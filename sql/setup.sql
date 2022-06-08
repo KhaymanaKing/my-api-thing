1419eaa76e45e7b2fc9b54e2db7f08e3ed38318a
Khayman King <khaymanak@gmail.com>
2022-06-08 14:48:50 -0700

    started data

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,34 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists bears;
+CREATE table bears (
+    id BIGINT GENERATED ALWAYS AS IDENITY,
+    name VARCHAR,
+    type VARCHAR,
+    color VARCHAR,
+    willEatFace BOOLEAN,
+    coolFactor INT
+)
+
+INSERT INTO bears(name, type, color, willEatFace, coolFactor) VALUES 
+(
+    'Pooh Bear',
+    'Cartoon',
+    'Yellow',
+    'false',
+    '6'
+),
+(
+    'Black Bear',
+    'Actual Bear',
+    'Black',
+    'true',
+    '7'
+)
+(
+    'Brown Bear'
+    'Actual Bear'
+    'Brown',
+    'true',
+    
+)
