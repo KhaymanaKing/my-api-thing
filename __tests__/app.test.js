0d4f94c9c6254174f90507ff1cc2d052085e13e0
Khayman King <khaymanak@gmail.com>
2022-06-08 16:00:58 -0700

    changed files around attempting to get test to work

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,26 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Bear = require('../lib/models/bear');
 
-describe('backend-express-template routes', () => {
+describe('bears routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/bears should return a list of bears ', async () => {
+    const res = await request(app).get('/bears');
+    const bears = await Bear.getAll();
+    const expected = bears.map((bear) => {
+      return { id: bear.id, name: bear.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('/bears/:id should return bear details', async() => {
+    const res = await request(app).get('/bears/1');
+    const bear = await Bear.getById(1);
+    expect(res.body).toEqual(bear);
   });
   afterAll(() => {
     pool.end();
