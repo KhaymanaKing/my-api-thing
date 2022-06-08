8f51cdad78cd69609361f9f588980cd13e084784
Khayman King <khaymanak@gmail.com>
2022-06-08 15:07:17 -0700

    created my cats table

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,15 +2,15 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP table if exists bears;
 CREATE table bears (
-    id BIGINT GENERATED ALWAYS AS IDENITY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR,
     type VARCHAR,
     color VARCHAR,
     willEatFace BOOLEAN,
-    coolFactor INT
-)
+    coolFactor BIGINT
+);
 
-INSERT INTO bears(name, type, color, willEatFace, coolFactor) VALUES 
+INSERT INTO bears (name, type, color, willEatFace, coolFactor) VALUES 
 (
     'Pooh Bear',
     'Cartoon',
@@ -24,11 +24,32 @@ INSERT INTO bears(name, type, color, willEatFace, coolFactor) VALUES
     'Black',
     'true',
     '7'
-)
+),
 (
-    'Brown Bear'
-    'Actual Bear'
+    'Brown Bear',
+    'Actual Bear',
     'Brown',
     'true',
-    
-)
+    '4'
+),
+(
+    'Grizzly Bear',
+    'Actual Terrifying Bear',
+    'Brown',
+    'true',
+    '8'
+),
+(
+    'Yogi Bear',
+    'Cartoon Bear',
+    'Brown',
+    'false',
+    '3'
+),
+(
+    'Chicago Bear',
+    'Football Player',
+    'Orange',
+    'false',
+    '2'
+);
